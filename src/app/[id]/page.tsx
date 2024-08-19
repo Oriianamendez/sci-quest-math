6e8e7a5c9d1c377fad05be72ec6516fa77cea0c8
Oriana Mendez <egleemendez@gmail.com>
2024-08-19 15:07:48 +0200

    gh-5 mock questions and answers
    Co-authored-by: Lukas Laas <lukas.s.laas@gmail.com>
    Co-authored-by: Hano2001 <Haraldnorlin93@gmail.com>

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,12 +1,26 @@
 "use client";
 
+import { useState } from "react";
+
 export default function PupilPage({ params }: { params: { id: string } }) {
-  console.log(params);
+  const [problem, setProblem] = useState(0);
+
+  const mockProblems = [
+    {
+      problem: "1 + 1",
+      solution: 2,
+    },
+    {
+      problem: "2 + 2",
+      solution: 4,
+    },
+  ];
+
   return (
     <>
       <h1>{params.id}</h1>
       <h2>Questions</h2>
-      <p>{"problem"}</p>
+      <p>{mockProblems[problem].problem}</p>
       <form action="">
         <input type="number" placeholder="Answer" />
         <button type="submit">Check</button>
