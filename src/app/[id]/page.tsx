290d4a33ebb5193ab8d0d9da5f3d600bde49895f
lukas-laas <lukas.s.laas@gmail.com>
2024-08-19 15:00:14 +0200

    gh-5 skeleton for pupil page
    Co-authored-by: Orianamendez <egleemendez@gmail.com>
    Co-authored-by: Hano2001 <Haraldnorlin93@gmail.com>

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -2,5 +2,15 @@
 
 export default function PupilPage({ params }: { params: { id: string } }) {
   console.log(params);
-  return <h1>{params.id}</h1>;
+  return (
+    <>
+      <h1>{params.id}</h1>
+      <h2>Questions</h2>
+      <p>{"problem"}</p>
+      <form action="">
+        <input type="number" placeholder="Answer" />
+        <button type="submit">Check</button>
+      </form>
+    </>
+  );
 }
