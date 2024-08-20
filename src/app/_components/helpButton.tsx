9f15d3ee1d98a8d2cb35777103ba5d93f78bca92
Oriana Mendez <egleemendez@gmail.com>
2024-08-20 15:00:29 +0200

    add image for help button
    Co-authored-by: Lukas Laas <lukas.s.laas@gmail.com>
    Co-authored-by: Hano2001 <Haraldnorlin93@gmail.com>

--- src/app/_components/helpButton.tsx
+++ src/app/_components/helpButton.tsx
@@ -1,7 +1,19 @@
 "use client";
 
 import { pupilHelpAction } from "@/actions";
+import Image from "next/image";
 
 export function Help({ id }: { id: string }) {
-  return <button onClick={() => pupilHelpAction(id)}>HELP</button>;
+  return (
+    <button onClick={() => pupilHelpAction(id)}>
+      <Image
+        className="m-0"
+        src="/help.png"
+        priority
+        width={50}
+        height={50}
+        alt=""
+      />
+    </button>
+  );
 }
