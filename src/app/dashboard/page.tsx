befdd62bb9530b48b89f0e720c294418ab1c3ed0
Oriana Mendez <egleemendez@gmail.com>
2024-08-20 09:42:53 +0200

    gh-11 visual pupils table
    Co-authored-by: Lukas Laas <lukas.s.laas@gmail.com>
    Co-authored-by: Hano2001 <Haraldnorlin93@gmail.com>

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,45 @@
 export default function Dashboard() {
-  return <>Teacher view</>;
+  const mockPupils = [
+    {
+      id: 1,
+      name: "Harald",
+      points: 3,
+    },
+    {
+      id: 2,
+      name: "Lukas",
+      points: 3,
+    },
+    {
+      id: 3,
+      name: "Oriana",
+      points: 3,
+    },
+  ];
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Points</th>
+            <th>Help</th>
+          </tr>
+        </thead>
+        <tbody>
+          {mockPupils.map((pupil) => (
+            <tr key={pupil.id}>
+              <td>{pupil.name}</td>
+              <td>{pupil.points}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <form action="">
+        <input type="text" placeholder="Pupils name" />
+        <button>Add</button>
+      </form>
+    </>
+  );
 }
