40594fd2d1674fdf5cdd23c531951f9f5320e2ab
lukas-laas <lukas.s.laas@gmail.com>
2024-08-20 13:25:03 +0200

    add key to status bar

--- src/app/_components/points-bar.tsx
+++ src/app/_components/points-bar.tsx
@@ -5,11 +5,17 @@ export function PointsBar(problems: any[], points: number) {
     <div className="flex gap-1 items-center">
       {problems.map((problem, i) =>
         points > i ? (
-          <div className="bg-emerald-600 text-white rounded-full size-6 flex items-center">
+          <div
+            key={problem.id}
+            className="bg-emerald-600 text-white rounded-full size-6 flex items-center"
+          >
             <CheckIcon />
           </div>
         ) : (
-          <div className="bg-neutral-500 rounded-full size-6" />
+          <div
+            key={problem.id}
+            className="bg-neutral-500 rounded-full size-6"
+          />
         )
       )}
       <div className="text-3xl bg-emerald-600 text-white size-10 rounded-full flex items-center justify-center">
