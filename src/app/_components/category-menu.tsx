9abc0a2e2511ce149cbec937fa5d4aeba80efb08
lukas-laas <lukas.s.laas@gmail.com>
2024-08-21 11:48:19 +0200

    update params for category
    Co-authored-by: Orianamendez <egleemendez@gmail.com>
    Co-authored-by: Hano2001 <Haraldnorlin93@gmail.com>

--- src/app/_components/category-menu.tsx
+++ src/app/_components/category-menu.tsx
@@ -1,18 +1,38 @@
+"use client";
 import { Rubik_Bubbles } from "next/font/google";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 const rubik = Rubik_Bubbles({ weight: ["400"], subsets: ["latin"] });
 export function CategoryMenu() {
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const router = useRouter();
+  function setCategory(id: string) {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("category", id);
+    router.push(pathName + "?" + params);
+  }
+
   return (
     <aside
       className={`${rubik.className} flex flex-col text-5xl gap-8 text-white`}
     >
-      <button className="bg-sky-300 size-20 flex items-center justify-center rounded-lg">
+      <button
+        className="bg-sky-300 size-20 flex items-center justify-center rounded-lg"
+        onClick={() => setCategory("1")}
+      >
         +
       </button>
-      <button className="bg-sky-300 size-20 flex items-center justify-center rounded-lg">
+      <button
+        className="bg-sky-300 size-20 flex items-center justify-center rounded-lg"
+        onClick={() => setCategory("2")}
+      >
         -
       </button>
-      <button className="bg-sky-300 size-20 flex items-center justify-center rounded-lg">
+      <button
+        className="bg-sky-300 size-20 flex items-center justify-center rounded-lg"
+        onClick={() => setCategory("3")}
+      >
         ?
       </button>
     </aside>
