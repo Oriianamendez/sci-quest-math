7c211ef789a767f617d63317bcc8c6c87879eecc
lukas-laas <lukas.s.laas@gmail.com>
2024-08-21 10:14:56 +0200

    update questions
    Co-authored-by: Orianamendez <egleemendez@gmail.com>
    Co-authored-by: Hano2001 <Haraldnorlin93@gmail.com>

--- src/queries.ts
+++ src/queries.ts
@@ -1,7 +1,6 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
-import { revalidatePath } from "next/cache";
 
 const url = process.env.POSTGRES_URL as string;
 
@@ -18,10 +17,57 @@ type Pupil = {
 const mockProblems = [
   {
     id: "1",
+    categoryId: "1",
     problem: "1 + 1",
     solution: 2,
   },
-  { id: "2", problem: "2 + 2", solution: 4 },
+  { id: "2", categoryId: "1", problem: "2 + 2", solution: 4 },
+  { id: "3", categoryId: "1", problem: "3 + 3", solution: 6 },
+  { id: "4", categoryId: "2", problem: "4 - 2", solution: 2 },
+  { id: "5", categoryId: "3", problem: "10 - 4", solution: 6 },
+  { id: "6", categoryId: "4", problem: "8 - 4", solution: 4 },
+  {
+    id: "7",
+    categoryId: "3",
+    problem:
+      "You have 3 apples. Your friend gives you 4 more apples. How many apples do you have now?",
+    solution: 7,
+  },
+  {
+    id: "8",
+    categoryId: "3",
+    problem:
+      "There are 5 birds on a tree. 2 more birds come and join them. How many birds are on the tree now?",
+    solution: 7,
+  },
+  {
+    id: "9",
+    categoryId: "3",
+    problem:
+      "You have 6 toy cars. You get 3 more toy cars as a gift. How many toy cars do you have in total?",
+    solution: 9,
+  },
+  {
+    id: "10",
+    categoryId: "4",
+    problem:
+      "You have 8 candies. You eat 3 of them. How many candies do you have left?",
+    solution: 5,
+  },
+  {
+    id: "11",
+    categoryId: "4",
+    problem:
+      "There are 7 balloons. 2 balloons fly away. How many balloons are left?",
+    solution: 5,
+  },
+  {
+    id: "12",
+    categoryId: "4",
+    problem:
+      "You have 10 cookies. You give 4 cookies to your friend. How many cookies do you have now?",
+    solution: 6,
+  },
 ];
 const mockPupils: Pupil[] = [
   {
