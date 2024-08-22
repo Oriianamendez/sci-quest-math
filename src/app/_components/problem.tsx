61c941489fceae142be5c54fa3ed8806d8e01d60
lukas-laas <lukas.s.laas@gmail.com>
2024-08-22 14:13:01 +0200

    reset answer form after submit

--- src/app/_components/problem.tsx
+++ src/app/_components/problem.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { updatePupilAction } from "@/actions";
+import { useRef, useState } from "react";
 
 export function Problem({ problems, pupil }: { problems: any[]; pupil: any }) {
+  const formRef = useRef<HTMLFormElement>(null);
+  const [formLoading, setFormLoading] = useState(false);
   const currentProblem = problems.find(
     (problem) => !pupil.answers.includes(problem.id)
   );
@@ -10,29 +13,51 @@ export function Problem({ problems, pupil }: { problems: any[]; pupil: any }) {
   if (!currentProblem) return "Finished";
 
   function correctAnswer(formData: FormData) {
+    setFormLoading(true);
     if ((formData.get("answer") as string) == currentProblem.solution) {
-      return updatePupilAction(pupil.id, currentProblem.id);
+      setTimeout(() => {
+        updatePupilAction(pupil.id, currentProblem.id);
+        setFormLoading(false);
+        formRef.current?.reset();
+        return;
+      }, 1000);
     }
   }
-  //if (pupil.points >= problems.length) return <p>Finished.</p>;
+
   return (
     <>
-      <p className="text-justify text-black font-extrabold text-7xl">
+      <p className="text-justify text-black font-extrabold text-2xl lg:text-4-xl">
         {currentProblem.problem}
       </p>
-      <form action={correctAnswer} className="flex gap-2">
+      <form
+        ref={formRef}
+        action={async (formData) => {
+          correctAnswer(formData);
+        }}
+        className="flex gap-2"
+      >
         <input
           name="answer"
           type="number"
           placeholder="Answer"
           className="border border-black rounded-md p-2"
         />
-        <button
-          type="submit"
-          className="bg-sky-200 hover:bg-sky-100 p-2 rounded-md"
-        >
-          Check
-        </button>
+        {formLoading ? (
+          <button
+            type="button"
+            disabled
+            className="bg-sky-200 hover:bg-sky-100 p-2 rounded-md"
+          >
+            Loading
+          </button>
+        ) : (
+          <button
+            type="submit"
+            className="bg-sky-200 hover:bg-sky-100 p-2 rounded-md"
+          >
+            Check
+          </button>
+        )}
       </form>
     </>
   );
