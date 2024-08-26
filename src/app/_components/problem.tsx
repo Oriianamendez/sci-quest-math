d4d7c2d7775db4b867c4bd0dffebcda7f1c99b64
lukas-laas <lukas.s.laas@gmail.com>
2024-08-26 15:18:03 +0200

    visualize wrong answer

--- src/app/_components/problem.tsx
+++ src/app/_components/problem.tsx
@@ -2,10 +2,13 @@
 
 import { updatePupilAction } from "@/actions";
 import { useRef, useState } from "react";
+import CloseIcon from "@mui/icons-material/Close";
 
 export function Problem({ problems, pupil }: { problems: any[]; pupil: any }) {
   const formRef = useRef<HTMLFormElement>(null);
   const [formLoading, setFormLoading] = useState(false);
+  const [wrongAnswer, setWrongAnswer] = useState(false);
+
   const currentProblem = problems.find(
     (problem) => !pupil.answers.includes(problem.id)
   );
@@ -14,14 +17,19 @@ export function Problem({ problems, pupil }: { problems: any[]; pupil: any }) {
 
   function correctAnswer(formData: FormData) {
     setFormLoading(true);
-    setTimeout(() => {
-      if ((formData.get("answer") as string) == currentProblem.solution) {
-        updatePupilAction(pupil.id, currentProblem.id);
-      }
-      setFormLoading(false);
-      formRef.current?.reset();
-      return;
-    }, 1000);
+    if ((formData.get("answer") as string) == currentProblem.solution) {
+      updatePupilAction(pupil.id, currentProblem.id);
+    } else {
+      setWrongAnswer(true);
+      return setTimeout(() => {
+        setWrongAnswer(false);
+        setFormLoading(false);
+        formRef.current?.reset();
+      }, 1500);
+    }
+    setFormLoading(false);
+    formRef.current?.reset();
+    return;
   }
 
   return (
@@ -46,14 +54,16 @@ export function Problem({ problems, pupil }: { problems: any[]; pupil: any }) {
           <button
             type="button"
             disabled
-            className="bg-sky-200 hover:bg-sky-100 text-black p-2 rounded-md"
+            className={`bg-sky-200 hover:bg-sky-100 text-black p-2 w-20 rounded-md ${
+              wrongAnswer && "bg-red-600 text-white hover:bg-red-600"
+            }`}
           >
-            Loading
+            {wrongAnswer ? <CloseIcon /> : "Loading"}
           </button>
         ) : (
           <button
             type="submit"
-            className="bg-sky-200 hover:bg-sky-100 text-black p-2 rounded-md"
+            className="bg-sky-200 hover:bg-sky-100 text-black p-2 w-20   rounded-md"
           >
             Check
           </button>
