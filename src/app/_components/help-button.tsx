266732ab5a7900dfcb52e01903d4a3f1c56130a1
Oriana Mendez <egleemendez@gmail.com>
2024-08-26 14:00:52 +0200

    gh-52 add visual feedback to help button
    Co-authored-by: Lukas Laas <lukas.s.laas@gmail.com>
    Co-authored-by: Hano2001 <Haraldnorlin93@gmail.com>

--- src/app/_components/help-button.tsx
+++ src/app/_components/help-button.tsx
@@ -1,13 +1,19 @@
 "use client";
 
 import { pupilHelpAction } from "@/actions";
+import { toast } from "@/components/ui/use-toast";
 import Image from "next/image";
 
 export function HelpButton({ id, help }: { id: string; help: number }) {
   return (
     <button
       className="rounded-full p-4 bg-neutral-500 hover:bg-green-500"
-      onClick={() => pupilHelpAction(id, help)}
+      onClick={() => {
+        pupilHelpAction(id, help),
+          toast({
+            description: "Help is on the way!",
+          });
+      }}
     >
       <Image
         className="m-0"
