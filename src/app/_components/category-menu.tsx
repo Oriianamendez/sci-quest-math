964022949938f3a0f173fa32d641967d939391d0
Oriana Mendez <egleemendez@gmail.com>
2024-08-27 14:23:16 +0200

    make it mobile responsive
    Co-authored-by: Lukas Laas <lukas.s.laas@gmail.com>
    Co-authored-by: Hano2001 <Haraldnorlin93@gmail.com>

--- src/app/_components/category-menu.tsx
+++ src/app/_components/category-menu.tsx
@@ -15,86 +15,86 @@ export function CategoryMenu() {
   }
   return (
     <aside
-      className={`${concert.className} flex flex-col text-7xl gap-4 p-4 pt-6`}
+      className={`${concert.className} flex flex-col gap-4 p-2 md:p-4 pt-6`}
     >
       <button
-        className="flex items-center justify-center rounded-lg"
+        className="flex items-center justify-center"
         onClick={() => setCategory("1")}
       >
         <Image
-          className="m-0"
+          className="m-0 size-10 md:size-16"
           src="/symbols/+easy.png"
           width={70}
           height={70}
-          alt="Plus symbol"
+          alt="Green plus symbol"
         />
       </button>
       <button
-        className="flex items-center justify-center rounded-lg"
+        className="flex items-center justify-center"
         onClick={() => setCategory("2")}
       >
         <Image
-          className="m-0"
+          className="m-0 size-10 md:size-16"
           src="/symbols/+medium.png"
           width={70}
           height={70}
-          alt="Minus symbol"
+          alt="Yellow plus symbol"
         />
       </button>
       <button
-        className="flex items-center justify-center rounded-lg"
+        className="flex items-center justify-center"
         onClick={() => setCategory("3")}
       >
         <Image
-          className="m-0"
+          className="m-0 size-10 md:size-16"
           src="/symbols/+difficult.png"
           width={70}
           height={70}
-          alt="Question mark symbol"
+          alt="Red plus symbol"
         />
       </button>
       <button
-        className="flex items-center justify-center rounded-lg"
+        className="flex items-center justify-center"
         onClick={() => setCategory("4")}
       >
         <Image
-          className="m-0"
+          className="m-0 size-10 md:size-16"
           src="/symbols/-easy.png"
           width={70}
           height={70}
-          alt="Question mark symbol"
+          alt="Green minus symbol"
         />
       </button>
       <button
-        className="flex items-center justify-center rounded-lg"
+        className="flex items-center justify-center"
         onClick={() => setCategory("5")}
       >
         <Image
-          className="m-0"
+          className="m-0 size-10 md:size-16"
           src="/symbols/-medium.png"
           width={70}
           height={70}
-          alt="Question mark symbol"
+          alt="Yellow minus symbol"
         />
       </button>
       <button
-        className="flex items-center justify-center rounded-lg"
+        className="flex items-center justify-center"
         onClick={() => setCategory("6")}
       >
         <Image
-          className="m-0"
+          className="m-0 size-10 md:size-16"
           src="/symbols/-difficult.png"
           width={70}
           height={70}
-          alt="Question mark symbol"
+          alt="Red minus symbol"
         />
       </button>
       <button
-        className="flex items-center justify-center rounded-lg"
+        className="flex items-center justify-center"
         onClick={() => setCategory("7")}
       >
         <Image
-          className="m-0"
+          className="m-0 size-10 md:size-16"
           src="/symbols/question-mark.png"
           width={70}
           height={70}
