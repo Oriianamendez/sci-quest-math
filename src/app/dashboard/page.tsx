1292fdc07673d936e747d56889acf6d697e57888
Hano2001 <haraldnorlin93@gmail.com>
2024-08-28 13:53:59 +0200

    wrap dashboard in signedin/signedout
    Co-authored-by: Orianamendez <egleemendez@gmail.com>
    Co-authored-by: Lukaslaas <lukas.s.laas@gmail.com>

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,19 +4,27 @@ import PupilsTable from "../_components/pupils-table";
 import { Navbar } from "../_components/navbar";
 import { Main } from "../_components/main";
 import { CategoryLegend } from "../_components/category-legend";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export default async function Dashboard() {
   return (
     <div className="bg-sky-100 min-h-screen">
       <Main>
         <Navbar />
-        <div className="flex justify-around pt-6">
-          <CategoryLegend />
-          <div className="flex flex-col items-center basis-5/6 h-[75vh]">
-            <PupilsTable />
-            <AddPupilForm />
+        <SignedIn>
+          <div className="flex justify-around pt-6">
+            <CategoryLegend />
+            <div className="flex flex-col items-center basis-5/6 h-[75vh]">
+              <PupilsTable />
+              <AddPupilForm />
+            </div>
           </div>
-        </div>
+        </SignedIn>
+        <SignedOut>
+          <div className="flex justify-center items-center">
+            <h1>Please sign in</h1>
+          </div>
+        </SignedOut>
       </Main>
     </div>
   );
